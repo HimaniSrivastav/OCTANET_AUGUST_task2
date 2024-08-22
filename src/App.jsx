c58aa3c3651dc89136109f03cc6bf794ec6a8223
HimaniSrivastav <132381750+HimaniSrivastav@users.noreply.github.com>
2024-08-22 20:50:47 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ToDo from './component/ToDo';
+function App() {
+  return (
+    <div className='bg-slate-900 grid py-4 min-h-screen'>
+    <ToDo/>
+    </div>
+  );
+}
+
+export default App;
